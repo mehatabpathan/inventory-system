111361f8b77f66e0d2567d83c544e892b0d92d05
mehatabpathan <>
2023-09-18 09:54:00 +0000

    adding requiremnets and writing print statemnent in run.py

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('''
+--------------------------
+Welcome to ABC Electronics
+--------------------------
+1.View Items
+2.Buy Items
+3.Exit
+''')
