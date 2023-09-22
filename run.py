44ea2c4e084e933b74b2213953a8f63e4069edcf
mehatabpathan <>
2023-09-22 13:46:55 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
-print('''
---------------------------
-Welcome to ABC Electronics
---------------------------
-1.View Items
-2.Buy Items
-3.Exit
-''')
-choice = int(input("Enter Your Option: "))
-if choice == 1:
-    print("Show Items!!!")
+"""
+Defines the main() function which starts the game, along with the Game class
+which is responsible for controlling the flow of the game.
+"""
 
+from datetime import date
+import os
+import random
+import gspread
+from google.oauth2.service_account import Credentials
+import pyfiglet
+import pandas as pd
+import colorama
+from colorama import Fore, Style
+from modules.word_checker import WordChecker
+
+colorama.init(autoreset=True)
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('word-Py-Leaderboard')
