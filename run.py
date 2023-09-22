db52fc0d6acf97c99b5365d182caeb31b15f23e8
mehatabpathan <>
2023-09-22 16:33:00 +0000

    importing date and time and figlet

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import random
+import sys
+import pyfiglet
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -7,7 +11,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('/Users/mehatab/Downloads/word-Py-main/testing/creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('products')
