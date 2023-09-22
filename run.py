d0fb2e5ede153fa1e4d4a98e90b1add35de808d3
mehatabpathan <>
2023-09-22 13:53:56 +0000

    creating googlespread and outh

--- run.py
+++ run.py
@@ -1,20 +1,5 @@
-"""
-Defines the main() function which starts the game, along with the Game class
-which is responsible for controlling the flow of the game.
-"""
-
-from datetime import date
-import os
-import random
 import gspread
 from google.oauth2.service_account import Credentials
-import pyfiglet
-import pandas as pd
-import colorama
-from colorama import Fore, Style
-from modules.word_checker import WordChecker
-
-colorama.init(autoreset=True)
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -25,4 +10,4 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('word-Py-Leaderboard')
+SHEET = GSPREAD_CLIENT.open('products')
